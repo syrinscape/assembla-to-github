f3c3d960234351b7b988bf654ada8cff6118b5f3
Tai Lee <real.human@mrmachine.net>
2023-02-07 20:22:45 +1100

    Move assembla IDs to settings.

--- Assembla_Github_v5.py
+++ Assembla_Github_v5.py
@@ -280,41 +280,7 @@ preformatted <b>code block</b>
 # assert assembla_to_gfm(before) == after, "Regexp test failure."
 
 
-val = {
-# add here your user and space_id of each contributor separated with a comma - for example
-    'addyyeow': 'aOZlLIPOur5kejacwqjQYw',
-    'aliceathens': 'a5cwOeFaSr5ioBacwqjQXA',
-    'Anonymous': 'bgfq4qA1Gr2QjIaaaHk9wZ',
-    'Aramgutang': 'bjBk7u6nWr3lUBabIlDkbG',
-    'Arianne.E': 'cRDJM0VPGr4OkNacwqjQWU',
-    'aweakley': 'di0DGS4Z8r3inTabIlDkbG',
-    'BenjaminLoomes': 'buE2R2Ooar4ONdacwqEsg8',
-    'cogat': 'dCkEYsHP0r3icQabIlDkbG',
-    'DrMeers': 'alce22Iair366feJe5cbCb',
-    'Fabianmcdonald': 'b-NzA8r-mr5RK_dmr6CpXy',
-    'jamesmurty': 'agL5-eYU8r4BknacwqjQXA',
-    'jonhuber': 'bdreHwH30r4OVdacwqjQXA',
-    'kaveht': 'cDXunmL1ur56dcacwqEsg8',
-    'lostsheep007': 'a0Fv1cp8ur5ikCacwqjQYw',
-    'Mark Finger': 'cd5j9MQNGr4lvReJe5cbCb',
-    'mattjg': 'bIWj8okVmr7jJccP_HzTya',
-    'mattoc': 'axJJ9e0WGr4i0teJe5cbLr',
-    'mike@interaction.net.au': 'aeZvakTQir4OkNacwqjQXA',
-    'mjog': 'bhVijwJ4yr46e8acwqjQWU',
-    'mrchantey': 'dLb_rCGiyr64kgbK8JiBFu',
-    'ominousbarry': 'dL_FPIV50r7iNccK-zJOy8',
-    'patarmstrong': 'dv2u8uqFir5ljwacwqEsg8',
-    'rantecki': 'd30d3S97Kr4y8DacwqjQWU',
-    'rl-0x0': 'buJ90CdH4r4ls7eJe5cbLr',
-    'ryan.cassar': 'dGzpJ6Giyr65ddcK-zJOy8',
-    'ryan.stuart': 'a-hEzew8mr5yo6dmr6bg7m',
-    'sam_mi': 'bazo9all4r5iepdmr6bg7m',
-    'simon@redcrowdigital.com.au': 'dMoZ4C9vur4z-OacwqjQYw',
-    'sjdines': 'a637DGkwur463cacwqjQYw',
-    'Syrin-Chris': 'dyQVs0V50r7k4saIC_Qgzw',
-    'tailee': 'cRu3xiRvGr44oYacwqEsg8',
-    'timjmansfield': 'chrW-oQZKr5jJcdmr6bg7m',
-}
+assembla_ids = Settings.assembla_ids
 
 KB = 1000
 MB = KB * 1000
@@ -1139,12 +1105,12 @@ def main():
 
     # add usernames to tickets according to user ids
     for ticket in sorted_tickets_array:
-        for key in val.keys():
-            if ticket["ticket_reporter_id"] == val[key]:
+        for key in assembla_ids.keys():
+            if ticket["ticket_reporter_id"] == assembla_ids[key]:
                 ticket["ticket_username"] = key
         for comment in ticket["ticket_comments"]:
-            for key in val.keys():
-                if comment["user_id"] == val[key]:
+            for key in assembla_ids.keys():
+                if comment["user_id"] == assembla_ids[key]:
                     comment["username"] = key
 
     # Get issues one time only.
--- settings.py
+++ settings.py
@@ -2,6 +2,41 @@ class Settings():
     github_user = "..."
     github_password = "..."
     github_token = "..."
+    assembla_ids = {
+        # Add the user and space_id of each contributor.
+        'addyyeow': 'aOZlLIPOur5kejacwqjQYw',
+        'aliceathens': 'a5cwOeFaSr5ioBacwqjQXA',
+        'Anonymous': 'bgfq4qA1Gr2QjIaaaHk9wZ',
+        'Aramgutang': 'bjBk7u6nWr3lUBabIlDkbG',
+        'Arianne.E': 'cRDJM0VPGr4OkNacwqjQWU',
+        'aweakley': 'di0DGS4Z8r3inTabIlDkbG',
+        'BenjaminLoomes': 'buE2R2Ooar4ONdacwqEsg8',
+        'cogat': 'dCkEYsHP0r3icQabIlDkbG',
+        'DrMeers': 'alce22Iair366feJe5cbCb',
+        'Fabianmcdonald': 'b-NzA8r-mr5RK_dmr6CpXy',
+        'jamesmurty': 'agL5-eYU8r4BknacwqjQXA',
+        'jonhuber': 'bdreHwH30r4OVdacwqjQXA',
+        'kaveht': 'cDXunmL1ur56dcacwqEsg8',
+        'lostsheep007': 'a0Fv1cp8ur5ikCacwqjQYw',
+        'Mark Finger': 'cd5j9MQNGr4lvReJe5cbCb',
+        'mattjg': 'bIWj8okVmr7jJccP_HzTya',
+        'mattoc': 'axJJ9e0WGr4i0teJe5cbLr',
+        'mike@interaction.net.au': 'aeZvakTQir4OkNacwqjQXA',
+        'mjog': 'bhVijwJ4yr46e8acwqjQWU',
+        'mrchantey': 'dLb_rCGiyr64kgbK8JiBFu',
+        'ominousbarry': 'dL_FPIV50r7iNccK-zJOy8',
+        'patarmstrong': 'dv2u8uqFir5ljwacwqEsg8',
+        'rantecki': 'd30d3S97Kr4y8DacwqjQWU',
+        'rl-0x0': 'buJ90CdH4r4ls7eJe5cbLr',
+        'ryan.cassar': 'dGzpJ6Giyr65ddcK-zJOy8',
+        'ryan.stuart': 'a-hEzew8mr5yo6dmr6bg7m',
+        'sam_mi': 'bazo9all4r5iepdmr6bg7m',
+        'simon@redcrowdigital.com.au': 'dMoZ4C9vur4z-OacwqjQYw',
+        'sjdines': 'a637DGkwur463cacwqjQYw',
+        'Syrin-Chris': 'dyQVs0V50r7k4saIC_Qgzw',
+        'tailee': 'cRu3xiRvGr44oYacwqEsg8',
+        'timjmansfield': 'chrW-oQZKr5jJcdmr6bg7m',
+    }
     assembla_user = "..."
     assembla_password = "..."
     files_url = "..."
